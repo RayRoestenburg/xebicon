37a79b5723fd9a645f6de6b15005d9e0dd6c9796
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2013-06-04 13:28:24 +0200

    removed implementation

--- src/main/scala/com/goticks/TicketSeller.scala
+++ src/main/scala/com/goticks/TicketSeller.scala
@@ -2,75 +2,20 @@ package com.goticks
 
 import akka.actor.{PoisonPill, Actor}
 
-class TicketSeller extends Actor {
-  import TicketProtocol._
-  import context._
+// TicketSeller
+  // get   import TicketProtocol._
 
-  var tickets = Vector[Ticket]()
-  def receive = soldOut
+  // context
 
-  def selling:Receive = {
-    case BuyTicket =>
-      if (tickets.isEmpty) {
-        sender ! SoldOut
-        become(soldOut)
-      }
+  // tickets
 
-      tickets.headOption.foreach { ticket =>
-        tickets = tickets.tail
-        sender ! ticket
-      }
-    case GetEvents => sender ! tickets.size
-  }
+  //  receive = so
 
-  def soldOut:Receive = {
-    case BuyTicket => sender ! SoldOut
+  // selling
+     // buy ticket
+     // get events
 
-    case Tickets(newTickets) =>
-      tickets = tickets ++ newTickets
-      become(selling)
-
-    case GetEvents => sender ! 0
-  }
-}
-
-object TicketProtocol {
-  import spray.json._
-
-  case class Event(event:String, nrOfTickets:Int)
-
-  case object GetEvents
-
-  case class Events(events:List[Event])
-
-  case object EventCreated
-
-  case class TicketRequest(event:String)
-
-  case object SoldOut
-
-  case class Tickets(tickets:List[Ticket])
-
-  case object BuyTicket
-
-  case class Ticket(event:String, nr:Int)
-
-  //----------------------------------------------
-  // JSON
-  //----------------------------------------------
-
-  object Event extends DefaultJsonProtocol {
-    implicit val format = jsonFormat2(Event.apply)
-  }
-
-  object TicketRequest extends DefaultJsonProtocol {
-    implicit val format = jsonFormat1(TicketRequest.apply)
-  }
-
-  object Ticket extends DefaultJsonProtocol {
-    implicit val format = jsonFormat2(Ticket.apply)
-  }
-
-}
-
-
+  // soldOut
+     // buy ticket -> soldOut
+     // get events -> 0
+//
